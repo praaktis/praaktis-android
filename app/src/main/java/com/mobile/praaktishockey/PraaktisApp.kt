510ee0034aa1d467fd646ddbf44923a13eda2b2e
Saidjamol Jiyanhodjayev <s.jiyankhodjaev@gmail.com>
2020-04-28 16:47:48 +0500

    * fabric replaced with firebase crashlytics

--- app/src/main/java/com/mobile/praaktishockey/PraaktisApp.kt
+++ app/src/main/java/com/mobile/praaktishockey/PraaktisApp.kt
@@ -1,10 +1,8 @@
 package com.mobile.praaktishockey
 
 import android.app.Application
-import com.crashlytics.android.Crashlytics
 import com.jakewharton.threetenabp.AndroidThreeTen
 import com.mobile.praaktishockey.domain.common.pref.SettingsStorage
-import io.fabric.sdk.android.Fabric
 import timber.log.Timber
 
 class PraaktisApp : Application() {
@@ -12,7 +10,6 @@ class PraaktisApp : Application() {
     override fun onCreate() {
         super.onCreate()
         app = this
-        Fabric.with(this, Crashlytics())
         AndroidThreeTen.init(this)
         if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
         initPreferences()
