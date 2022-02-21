f753da3ed4a4ff2a14ec76908c95ae05cfb77a40
Saidjamol Jiyankhodjaev <s.jiyankhodjaev@tengebank.uz>
2022-02-21 16:22:20 +0500

    - detailed analysis page

---
+++ app/src/main/java/com/mobile/gympraaktis/domain/common/DropdownView.kt
@@ -0,0 +1,13 @@
+package com.mobile.gympraaktis.domain.common
+
+import android.content.Context
+import android.util.AttributeSet
+import com.google.android.material.textfield.TextInputLayout
+
+class DropdownView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null
+) : TextInputLayout(context, attrs) {
+
+
+
+}
--- app/src/main/java/com/mobile/gympraaktis/ui/challenge/DetailAnalysisFragment.kt
+++ app/src/main/java/com/mobile/gympraaktis/ui/challenge/DetailAnalysisFragment.kt
@@ -1,15 +1,8 @@
 package com.mobile.gympraaktis.ui.challenge
 
 import android.os.Bundle
-import android.view.Gravity
-import android.view.ViewGroup
-import android.widget.LinearLayout
-import androidx.annotation.Dimension
-import androidx.appcompat.widget.AppCompatTextView
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import com.mobile.gympraaktis.R
 import com.mobile.gympraaktis.base.BaseFragment
 import com.mobile.gympraaktis.data.entities.AttemptEntity
@@ -17,7 +10,6 @@ import com.mobile.gympraaktis.databinding.FragmentDetailedAnalysisBinding
 import com.mobile.gympraaktis.databinding.LayoutTargetTimelineBinding
 import com.mobile.gympraaktis.domain.common.AnalysisLineChart
 import com.mobile.gympraaktis.domain.common.AppGuide
-import com.mobile.gympraaktis.domain.common.GRADIENT_PROGRESS_ARRAY
 import com.mobile.gympraaktis.domain.common.resettableLazy
 import com.mobile.gympraaktis.domain.entities.ChallengeDTO
 import com.mobile.gympraaktis.domain.entities.DetailPoint
@@ -31,7 +23,9 @@ import com.takusemba.spotlight.Target
 import kotlinx.android.synthetic.main.fragment_detailed_analysis.*
 import timber.log.Timber
 import java.util.*
-import kotlin.collections.HashMap
+import kotlin.collections.component1
+import kotlin.collections.component2
+import kotlin.collections.set
 
 class DetailAnalysisFragment constructor(override val layoutId: Int = R.layout.fragment_detailed_analysis) :
     BaseFragment<FragmentDetailedAnalysisBinding>() {
@@ -70,14 +64,16 @@ class DetailAnalysisFragment constructor(override val layoutId: Int = R.layout.f
     override fun initUI(savedInstanceState: Bundle?) {
         initToolbar()
         if (arguments?.get("score") != null) {
+            binding.cvSelectPlayer.show()
             mViewModel.getDetailResult(scoreDTO.attemptId) // from remote
         } else {
+            binding.cvSelectPlayer.hide()
             setDetail(collectDetailScores()) // from praaktis_sdk
         }
-        mViewModel.detailResultEvent.observe(this, Observer {
+        mViewModel.detailResultEvent.observe(viewLifecycleOwner) {
             setDetail(it)
             startGuideIfNecessary(it.size)
-        })
+        }
     }
 
     private fun collectDetailScores(): List<DetailScoreDTO> {
@@ -119,31 +115,19 @@ class DetailAnalysisFragment constructor(override val layoutId: Int = R.layout.f
     }
 
     private fun setDetail(detailScores: List<DetailScoreDTO>) {
-        val tvDragFlick = AppCompatTextView(requireContext())
-        if (arguments?.getSerializable("score") != null) tvDragFlick.text = scoreDTO.challengeName
-        else tvDragFlick.text = challengeItem.name
-        tvDragFlick.isAllCaps = true
-        tvDragFlick.setTextSize(Dimension.SP, 18f)
-        tvDragFlick.setTextColor(ContextCompat.getColor(requireContext(), R.color.purple_900_1))
-        val lp = LinearLayout.LayoutParams(
-            ViewGroup.LayoutParams.WRAP_CONTENT,
-            ViewGroup.LayoutParams.WRAP_CONTENT
-        )
-        lp.gravity = Gravity.CENTER_HORIZONTAL
-        lp.setMargins(0, 0, 0, 12.dp)
-        tvDragFlick.layoutParams = lp
-        llAnalysisContainer.addView(tvDragFlick)
+        if (arguments?.getSerializable("score") != null) binding.tvExerciseName.text = scoreDTO.challengeName
+        else binding.tvExerciseName.text = challengeItem.name
 
-        var gradientIterator = GRADIENT_PROGRESS_ARRAY.listIterator()
+//        var gradientIterator = GRADIENT_PROGRESS_ARRAY.listIterator()
 
         for (i in detailScores.indices) {
-            val progressBackground =
-                if (gradientIterator.hasNext())
-                    gradientIterator.next()
-                else {
-                    gradientIterator = GRADIENT_PROGRESS_ARRAY.listIterator()
-                    gradientIterator.next()
-                }
+//            val progressBackground =
+//                if (gradientIterator.hasNext())
+//                    gradientIterator.next()
+//                else {
+//                    gradientIterator = GRADIENT_PROGRESS_ARRAY.listIterator()
+//                    gradientIterator.next()
+//                }
 
             Timber.d(detailScores.toString())
 
@@ -151,7 +135,7 @@ class DetailAnalysisFragment constructor(override val layoutId: Int = R.layout.f
                 requireContext(),
                 detailScores[i].detailPointScore,
                 detailScores[i].detailPoint.name,
-                progressBackground,
+                R.drawable.gradient_progress,
                 detailScores[i].maxValue
             )
             llAnalysisContainer.addView(chart)
