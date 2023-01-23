7c1617ea3a8401d0b8928d394d5d8b51d7637cec
juraev <kh.j.mx@mail.ru>
2023-01-24 01:28:33 +0800

    added contents for boxiing

--- app/src/main/java/com/mobile/gympraaktis/domain/Constants.kt
+++ app/src/main/java/com/mobile/gympraaktis/domain/Constants.kt
@@ -2,7 +2,7 @@ package com.mobile.gympraaktis.domain
 
 class Constants {
     companion object {
-        const val ROUTINE_ID = 103
+        const val ROUTINE_ID = 34
         const val SINGLE_USER_MODE = true
         const val SDK_REQUEST_CODE = 333
     }
---
+++ countrypicker/build/generated/source/buildConfig/debug/com/mukesh/countrypicker/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.mukesh.countrypicker;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.mukesh.countrypicker";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
+  public static final String APPLICATION_ID = "com.mukesh.countrypicker";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 16;
+  public static final String VERSION_NAME = "2.0.1";
+}
