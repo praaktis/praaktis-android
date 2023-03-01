3c11c7e8d6cd04c696eb0368ee50bebbe46f0d54
juraev <kh.j.mx@mail.ru>
2023-03-01 13:53:16 +0500

    released wall slides v1.1

--- app/src/main/java/com/mobile/gympraaktis/domain/Constants.kt
+++ app/src/main/java/com/mobile/gympraaktis/domain/Constants.kt
@@ -2,7 +2,7 @@ package com.mobile.gympraaktis.domain
 
 class Constants {
     companion object {
-        const val ROUTINE_ID = 34
+        const val ROUTINE_ID = 110
         const val SINGLE_USER_MODE = true
         const val SDK_REQUEST_CODE = 333
     }
