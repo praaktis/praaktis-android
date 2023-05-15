beefbe8e2918411e01411c228ecfb148893cc8ab
juraev <kh.j.mx@mail.ru>
2023-05-15 17:14:36 +0500

    updated sdk

--- app/src/main/java/com/mobile/gympraaktis/domain/Constants.kt
+++ app/src/main/java/com/mobile/gympraaktis/domain/Constants.kt
@@ -3,7 +3,7 @@ package com.mobile.gympraaktis.domain
 class Constants {
     companion object {
         const val ROUTINE_ID = 112
-        const val SINGLE_USER_MODE = false
+        const val SINGLE_USER_MODE = true
         const val SDK_REQUEST_CODE = 333
     }
 }
